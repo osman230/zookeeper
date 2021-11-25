b1527e8f2b5716d82109a7422321e697b59a3e33
Aisha Osman <aisha.osman09@gmail.com>
2021-11-25 16:17:49 -0600

    server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { application } = require('express');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,16 +8,14 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
 });
 
 
-
 app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}');
+    console.log('Server running on port 3001');
 });

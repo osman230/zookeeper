89c6152fe00bff3a1f418eb1225a1246023255c7
Aisha Osman <aisha.osman09@gmail.com>
2021-11-25 16:28:49 -0600

    Server

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const { application } = require('express');
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
+const mysql = require('mysql2');
 
 // express middleware
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//mysql connection
 
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        //Your Mysql username,
+        user: 'root',
+        //Your Mysql password
+        password:'',
+        database:'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
@@ -17,5 +30,5 @@ app.use((req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log('Server running on port 3001');
+    console.log('Server running on port 3002');
 });
